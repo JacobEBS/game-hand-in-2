b10a9ddb581f1b93620e7bd8814f365df2bde707
JacobEBS <91956508+JacobEBS@users.noreply.github.com>
2021-10-14 14:01:07 +0200

    Update main.blocks, main.ts, images.g.jres, images.g.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,26 +1,15 @@
-controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
-    mySprite2.vy = -100
-    mySprite2.ay = 100
-    mySprite.vy = -100
-    mySprite.ay = 100
-})
+namespace SpriteKind {
+    export const ground = SpriteKind.create()
+}
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
-    mySprite2.vx = 200
-    mySprite2.vy = -250
-    mySprite2.ay = 200
-    mySprite.vx = 200
-    mySprite.vy = -250
-    mySprite.ay = 200
+    if (mySprite.isHittingTile(CollisionDirection.Bottom)) {
+        mySprite.vy = -150
+    }
 })
-let mySprite2: Sprite = null
 let mySprite: Sprite = null
 mySprite = sprites.create(assets.image`Table main character`, SpriteKind.Player)
-mySprite2 = sprites.create(assets.image`Table main character0`, SpriteKind.Player)
-controller.moveSprite(mySprite)
-controller.moveSprite(mySprite2)
+controller.moveSprite(mySprite, 100, 0)
 tiles.setTilemap(tilemap`Tilemaplol`)
-scene.cameraFollowSprite(mySprite2)
+scene.cameraFollowSprite(mySprite)
 tiles.placeOnTile(mySprite, tiles.getTileLocation(5, 210))
-tiles.placeOnTile(mySprite2, tiles.getTileLocation(6, 210))
-mySprite2.ay = 300
 mySprite.ay = 300
